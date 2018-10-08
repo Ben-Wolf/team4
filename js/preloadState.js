5d3b75886f423294f14129ab96d457339a91f008
Benjamin Wolf <bwolfgns@gmail.com>
2018-10-08 04:17:23 -0400

    Added phase2 basic functionality (#1)

---
+++ js/preloadState.js
@@ -0,0 +1,16 @@
+// PRELOAD STATE
+let preloadState = function() {
+
+};
+
+preloadState.prototype.preload = function() {
+    game.load.image("star", "assets/star.png");
+	game.load.image("bound_h", "assets/bound_h.png");
+	game.load.image("bound_v", "assets/bound_v.png");
+    game.load.image("steering_wheel", "assets/bound_h.png");
+	game.load.spritesheet("murph", "assets/character.png", 32, 48);
+};
+
+preloadState.prototype.create = function() {
+    game.state.start("Game");
+};
