4e07834c9dcc5b4f7a0329f81e9ce31779bba012
Benjamin Wolf <bwolfgns@gmail.com>
2018-10-10 01:23:55 -0400

    updated all assets to fit new screen size

--- js/gameState.js
+++ js/gameState.js
@@ -16,49 +16,59 @@ gameState.prototype.create = function() {
     // Create the boundaries TODO: Make this adding in the boundaries, tilesets, etc...
 	this.bounds = game.add.group();
 	this.bounds.enableBody = true;
-	let bound_b = this.bounds.create(0, game.world.height - 600);
+	let bound_b = this.bounds.create(0, game.world.height - 600, "bound_h");
 	bound_b.scale.set(4, 2);
 	bound_b.body.immovable = true;
-    bound_b.tint = 0xff00ff;
-	let bound_t = this.bounds.create(0, 0);
-    bound_t.scale.set(4, 2);
+	bound_b.tint = 0x000000;
+	let bound_t = this.bounds.create(0, 0, "bound_h");
+	bound_t.scale.set(4, 2);
 	bound_t.body.immovable = true;
-    bound_t.tint = 0xff00ff;
-    let bound_l = this.bounds.create(100, 0);
-    bound_l.scale.set(2, 6.5);
-    bound_l.body.immovable = true;
-    bound_l.tint = 0xff00ff;
-    let bound_r = this.bounds.create(1000, 0);
-    bound_r.scale.set(2, 6.5);
-    bound_r.body.immovable = true;
-    bound_r.tint = 0xff00ff;
+	bound_t.tint = 0x000000;
+	let bound_l = this.bounds.create(0, 0, "bound_v");
+	bound_l.scale.set(2, 6.5);
+	bound_l.body.immovable = true;
+	bound_l.tint = 0x000000;
+	let bound_r = this.bounds.create(1000, 0, "bound_v");
+	bound_r.scale.set(2, 6.5);
+	bound_r.body.immovable = true;
+	bound_r.tint = 0x000000;
+
+	// Add HUD Area
+	this.hud_area = game.add.sprite(0, game.world.height - 550, "bound_h");
+	this.hud_area.scale.set(5, 18);
+	this.hud_area.tint = 0x000000;
 
     // Add "Steering wheel"
     this.wheel = game.add.sprite(210, game.world.height - 500, "steering_wheel");
     this.wheel.scale.set(1, 1);
-    var style = { font: "12px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: this.wheel.width, align: "center", backgroundColor: "#ffff00" };
-    var text = game.add.text(0, 0, "Steering Wheel", style);
-    text.anchor.set(0.5);
-    text.x = Math.floor(this.wheel.x + this.wheel.width / 2);
-    text.y = Math.floor(this.wheel.y + this.wheel.height / 2);
+    var style = { font: "32px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: this.wheel.width, align: "center", backgroundColor: "#ffff00" };
+	this.wheel.animations.add("right", [0, 1, 2, 3], 10, false);
+	this.wheel.animations.add("left", [0, 4, 5, 6], 10, false);
+	this.wheel.animations.add("reset_r", [3, 2, 1, 0], 10, false);
+	this.wheel.animations.add("reset_l", [6, 5, 4, 0], 10, false);
 
     // Create the cars TODO: Change star sprites to cars.
     this.cars = game.add.group();
     this.cars.enableBody = true;
-    this.speeds = [200, 400, 565, 680];
-    this.starts = [295, 510, 725];
+    this.speeds = [350, 400, 550, 600];
+    this.starts = [100, 440, 800];
     this.timers = [200, 120, 60];
-    createCars(this.cars, this.starts, this.speeds, this.timers);
+    createCars(this.cars, this.starts, this.speeds, this.timers, 1);
 
     // Create the player TODO: Change Murph to car
-    this.player = game.add.sprite(200, game.world.height - 850, "player");
-    this.player.scale.set(2,2);
+    this.player = game.add.sprite(200, game.world.height - 950, "player");
+    this.player.scale.set(3, 3);
 	game.physics.arcade.enable(this.player);
 	this.player.body.collideWorldBounds = true;
 
     // Distance to perp TODO: Implement more, based on speed
     this.d2p = 5000;
-    this.d2p_text = game.add.text(600, 300, this.d2p + "m from Perp", style);
+    this.d2p_text = game.add.text(0, 1900, this.d2p + "m from Perp", style);
+
+	// Speed modifiers
+	this.d2p_since_last_crash = this.d2p;
+	this.speed_multiplier = 1;
+	this.spm_text = game.add.text(0, 2100, this.speed_multiplier + " = Multiplier!", style);
 
     // Capture mouse input
     game.input.mouse.capture = true;
@@ -74,14 +84,10 @@ gameState.prototype.update = function() {
 	game.physics.arcade.overlap(this.bounds, this.cars, this.removeCar, null, this);
     game.physics.arcade.overlap(this.player, this.cars, this.crash, null, this);
 
-    // Update distance to perp text based on speed
-    // if (this.playerSpeed == TURNING) {
-    //     this.d2p -= 0.5;
-    // } else if (this.playerSpeed == STRAIGHT) {
-    //     this.d2p -= 1;
-    // } else if (this.playerSpeed == ACCELERATE) {
-    //     this.d2p -= 2;
-    // }
+	// Apply speed multipliers
+	this.speed_multiplier = Math.floor((this.d2p_since_last_crash - this.d2p) / 500) + 1;
+	this.spm_text.setText(this.speed_multiplier + " = Multiplier!");
+	this.d2p -= (0.5 * this.speed_multiplier);
 
     // TODO: Create and implement win state
     if (this.d2p <= 0) {
@@ -91,7 +97,7 @@ gameState.prototype.update = function() {
     }
 
     // Procedural Car Generation
-    createCars(this.cars, this.starts, this.speeds, this.timers);
+    createCars(this.cars, this.starts, this.speeds, this.timers, this.speed_multiplier);
 
     // Turn wheel
     this.player.body.velocity.x = 0;
@@ -103,7 +109,13 @@ gameState.prototype.update = function() {
                 this.turnRight();
             }
         }
-    }
+    } else {
+		if (this.wheel.animations.frame > 0 && this.wheel.animations.frame < 4){
+			this.wheel.animations.play("reset_r");
+		} else if (this.wheel.animations.frame > 3) {
+			this.wheel.animations.play("reset_l");
+		}
+	}
 };
 
 // Remove cars that reach the lower bounds
@@ -113,16 +125,24 @@ gameState.prototype.removeCar = function(boundary, car) {
 
 // Player crash
 gameState.prototype.crash = function(player, car) {
-    this.d2p += 500;
+	this.d2p += 500;
+	this.d2p_since_last_crash = this.d2p;
+	this.speed_multiplier = 1;
     this.cars.forEach(function (c) { c.kill(); });
 };
 
 gameState.prototype.turnRight = function() {
-    this.player.body.velocity.x = 275;
+	if (this.wheel.animations.frame !== 3) {
+		this.wheel.animations.play("right");
+	}
+    this.player.body.velocity.x = 335 * this.speed_multiplier;
 };
 
 gameState.prototype.turnLeft = function() {
-    this.player.body.velocity.x = -275;
+	if (this.wheel.animations.frame !== 6) {
+		this.wheel.animations.play("left");
+	}
+    this.player.body.velocity.x = -335 * this.speed_multiplier;
 };
 
 function inBounds(xIn, yIn, b) {
@@ -130,13 +150,13 @@ function inBounds(xIn, yIn, b) {
            yIn > b.y && yIn < (b.y + b.height);
 };
 
-function createCars(cars, starts, speeds, timers) {
+function createCars(cars, starts, speeds, timers, speed_m) {
     for (let i = 0; i < 3; ++i) {
-        if (timers[i] == 0) {
-            timers[i] = 250;
+        if (timers[i] === 0) {
+            timers[i] = 250/speed_m;
             let car = cars.create(starts[i], 65, "sedan");
-            car.body.velocity.y = speeds[getRandomInt(4)];
-            car.scale.set(2,2);
+            car.body.velocity.y = speeds[getRandomInt(4)] * speed_m;
+            car.scale.set(3,3);
         } else {
             timers[i]--;
         }
