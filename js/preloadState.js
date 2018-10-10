82aa1cc4d305cb7581f73348ef47a9a8cd1d69b9
Benjamin Wolf <bwolfgns@gmail.com>
2018-10-10 01:25:16 -0400

    updated all assets to fit new screen size (#4)

--- js/preloadState.js
+++ js/preloadState.js
@@ -4,8 +4,10 @@ let preloadState = function() {
 };
 
 preloadState.prototype.preload = function() {
-	game.load.image("star", "assets/star.png");
+	game.load.image("star", "assets/star.png"); // TODO: Make sure to remove unused assets
     game.load.image("sedan", "assets/sedan_sprite_1.png");
+	game.load.image("bound_h", "assets/bound_h.png");
+	game.load.image("bound_v", "assets/bound_v.png");
     game.load.spritesheet("steering_wheel", "assets/steering_wheel.png", 468, 468);
 	game.load.spritesheet("player", "assets/patrol_car.png", 64, 118);
 	game.load.tilemap("TileMap", "assets/map2.json", null, Phaser.Tilemap.TILED_JSON);
@@ -13,6 +15,6 @@ preloadState.prototype.preload = function() {
 };
 
 preloadState.prototype.create = function() {
-    //game.state.start("Title");
-    game.state.start("Game"); // Uncomment phase you're testing
+    game.state.start("Title");
+    // game.state.start("Game"); // Uncomment phase you're testing
 };
