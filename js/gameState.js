0971b535d185faa47e03abc1cfa098416ce77866
Stephen <frombs@rpi.edu>
2018-10-14 13:57:53 -0400

    Disabled animals for testing, changed car spawn times

--- js/gameState.js
+++ js/gameState.js
@@ -49,21 +49,22 @@ gameState.prototype.create = function() {
     this.cars = game.add.group();
     this.cars.enableBody = true;
     this.speeds = [350, 400, 550, 600];
-    this.starts = [150, 310, 450, 600, 770];
-    this.timers = [200, 120, 60, 90, 150];
+    this.starts = [150, 310, 450, 600, 760];
+    this.timers = [210, 160, 90, 120, 270];
 	this.sedans = ["sedan_red", "sedan_gray", "sedan_white"];
 	this.trucks = ["truck_red", "truck_gray", "truck_black", "truck_white"];
 	this.cargos = ["cargo_red", "cargo_gray", "cargo_green", "cargo_white"];
     createCars(this.cars, this.starts, this.speeds, this.timers, 1,
 				this.sedans, this.trucks, this.cargos);
 
-	//Create the animals
+	/*//Create the animals
 	this.animals = game.add.group();
 	this.animals.enableBody = true;
 	this.animalSpeeds = [100, 150, 200, 250];
 	this.animalStarts = [100, 440, 800];
 	this.animalTimers = [200, 120, 60];
 	createAnimals(this.animals, this.animalStarts, this.animalSpeeds, this.animalTimers, 1);
+	*/
 
     // Create the player TODO: Change Murph to car
     this.player = game.add.sprite(200, game.world.height - 950, "player");
@@ -72,7 +73,8 @@ gameState.prototype.create = function() {
 	this.player.body.collideWorldBounds = true;
 
     // Distance to perp TODO: Implement more, based on speed
-    this.d2p = 2000;
+    this.perpBool = false;
+	this.d2p = 2000;
     this.d2p_text = game.add.text(0, 1900, this.d2p + "m from Perp", style);
 
 	// Speed modifiers
@@ -88,24 +90,30 @@ gameState.prototype.update = function() {
 
     // Collisions
     game.physics.arcade.collide(this.bounds, this.player);
-    game.physics.arcade.collide(this.cars, this.cars);
-	game.physics.arcade.collide(this.animals, this.player);
+    //game.physics.arcade.collide(this.cars, this.cars);
+	//game.physics.arcade.collide(this.animals, this.player);
 
     // Overlaps
 	game.physics.arcade.overlap(this.bounds, this.cars, this.removeCar, null, this);
-	game.physics.arcade.overlap(this.bounds, this.animals, this.removeAnimal, null, this);
+	//game.physics.arcade.overlap(this.bounds, this.animals, this.removeAnimal, null, this);
     game.physics.arcade.overlap(this.player, this.cars, this.crash, null, this);
-	game.physics.arcade.overlap(this.player, this.animals, this.crash, null, this);
+	//game.physics.arcade.overlap(this.player, this.animals, this.crash, null, this);
 	game.physics.arcade.overlap(this.player, this.perp, this.win, null, this);
 
 	// Apply speed multipliers
-	this.speed_multiplier = Math.floor((this.d2p_since_last_crash - this.d2p) / 500) + 1;
+	if(!this.perpBool){
+		this.speed_multiplier = Math.floor((this.d2p_since_last_crash - this.d2p) / 500) + 1;
+	}
+	else{
+		this.speed_multiplier = 1;
+	}
 	this.spm_text.setText(this.speed_multiplier + " = Multiplier!");
 	this.d2p -= (0.5 * this.speed_multiplier);
 
     //
-    if (this.d2p <= 500) {
-		spawnPerp();
+    if (this.d2p <= 500 && !this.perpBool) {
+		spawnPerp(this.cars, this.starts);
+		this.perpBool = true;
     } else {
         this.d2p_text.setText(Math.round(this.d2p) + "m from Perp");
     }
@@ -115,7 +123,7 @@ gameState.prototype.update = function() {
 				this.speed_multiplier, this.sedans, this.trucks, this.cargos);
 
 	//Procedural Animal Generation
-	createAnimals(this.animals, this.animalStarts, this.animalSpeeds, this.animalTimers, this.speed_multiplier);
+	//createAnimals(this.animals, this.animalStarts, this.animalSpeeds, this.animalTimers, this.speed_multiplier);
 
     // Turn wheel
     this.player.body.velocity.x = 0;
@@ -140,10 +148,11 @@ gameState.prototype.update = function() {
 gameState.prototype.removeCar = function(boundary, car) {
     car.kill();
 };
-
+/*
 gameState.prototype.removeAnimal = function(boundary, animal) {
     animal.kill();
 };
+*/
 
 // Player crash
 gameState.prototype.crash = function(player, object) {
@@ -151,7 +160,7 @@ gameState.prototype.crash = function(player, object) {
 	this.d2p_since_last_crash = this.d2p;
 	this.speed_multiplier = 1;
     this.cars.forEach(function (c) { c.kill(); });
-	this.animals.forEach(function (a) { a.kill(); });
+	//this.animals.forEach(function (a) { a.kill(); });
 };
 
 gameState.prototype.turnRight = function() {
@@ -173,10 +182,10 @@ function inBounds(xIn, yIn, b) {
            yIn > b.y && yIn < (b.y + b.height);
 };
 
-function spawnPerp(perp, cars) {
-	perp = cars.create(start[2], 65, "sedan_red");
-	car.body.velocity.y = 50;
-	car.scale.set(1.5, 1.5);
+function spawnPerp(cars, start) {
+	let perp = cars.create(start[2], 65, "sedan_red");
+	perp.body.velocity.y = 50;
+	perp.scale.set(1.5, 1.5);
 };
 
 function createCars(cars, starts, speeds, timers, speed_m, s, t, c) {
@@ -195,7 +204,7 @@ function createCars(cars, starts, speeds, timers, speed_m, s, t, c) {
 			}
 
 			// Spawn the car
-            timers[i] = 200/speed_m;
+            timers[i] = 250/speed_m;
             let car = cars.create(starts[i], 65, type[getRandomInt(colors)]);
             car.body.velocity.y = speeds[getRandomInt(4)] * speed_m;
             car.scale.set(1.5,1.5);
@@ -204,19 +213,20 @@ function createCars(cars, starts, speeds, timers, speed_m, s, t, c) {
         }
     }
 };
-
+/*
 function createAnimals(animals, starts, speeds, timers, speed_m) {
     for (let i = 0; i < 3; ++i) {
         if (timers[i] === 0) {
-            timers[i] = 250/speed_m;
+            timers[i] = 120;
             let animal = animals.create(65, starts[i], "bird");
-            animal.body.velocity.x = - speeds[getRandomInt(4)] * speed_m;
+            animal.body.velocity.x = - 100;
             animal.scale.set(3,3);
         } else {
             timers[i]--;
         }
     }
 };
+*/
 
 // Bless JS Hoisting
 function getRandomInt(max) {
