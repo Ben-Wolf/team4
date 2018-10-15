d57abb3fbb4ac6501d2fad612bd45fc5111e836f
Sophie Richards <richas3@rpi.edu>
2018-10-15 16:22:31 -0400

    Added siren sound effect to the animation

--- js/preloadState.js
+++ js/preloadState.js
@@ -35,6 +35,8 @@ preloadState.prototype.preload = function() {
 	game.load.tilemap("TileMap2", "assets/map2.json", null, Phaser.Tilemap.TILED_JSON);
 	game.load.image("newtiles", "assets/newtiles.png");
 	game.load.image("curb", "assets/curb.png");
+	//sound effects
+	game.load.audio("siren","assets/siren.mp3");
 };
 
 preloadState.prototype.create = function() {
