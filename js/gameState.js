a29359ec4fa903f6e193332742e12fba33e46bcf
Stephen <frombs@rpi.edu>
2018-10-16 12:34:40 -0400

    Fixed merge error

--- js/gameState.js
+++ js/gameState.js
@@ -199,8 +199,6 @@ gameState.prototype.crash = function(player, object) {
 	this.numDeleted += 1;
 	this.haltAnimation = 90;
 	this.d2p += 350;
-    //car would continue to collide with the object, so now the object is killed on collision to prevent that
-    object.kill();
 	this.d2p_since_last_crash = this.d2p;
 	this.speed_multiplier = 1;
 	this.timers = [210, 160, 90, 120, 270];
